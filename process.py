a7804803a57fa5c7c8080442a367316e54005c65
herukurniawan <herupurnomokurniawan@gmail.com>
2019-04-02 13:58:05 +0700

    Change function to class

--- process.py
+++ process.py
@@ -1,5 +1,61 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-import pytesseract
+from PIL import Image
+import pytesseract as pyt
+import sys
+
+# Locate your tesseract executable
+pyt.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
+
+class pdf2bbox:
+
+    lang = 'eng'
+    config = '--psm 6 --oem 1'
+
+    def __init__(self, ipath):
+        # Image path is included while running python files (from Terminal)
+        imagepath = ipath
+
+        # Open image, get the width and height of the image
+        self.image = Image.open(imagepath)
+        self.width, self.height = self.image.size
+        
+        # Simple image to string, the converted text in english language.
+        self.text = pyt.image_to_string(self.image, lang=self.lang, config=self.config)
+
+        # Get data and normalize
+        self.prepare()
+        
+    
+    def prepare(self):
+        data = pyt.image_to_data(self.image, lang='eng', config='--psm 6 --oem 1')
+        self.data = [i.replace("\t",";") for i in data.split("\n")]
+        self.header = self.data.pop(0)
+        self.filterdata()
+        self.data = [self.data2bbox(i) for i in self.data]
+
+
+    def filterdata(self):
+        out = []
+        for i in self.data:
+            if not i.split(";")[11]:
+                continue
+            out.append(i)
+        self.data = out
+
+    def data2bbox(self,data):
+        data = data.split(";")
+
+        term = dict()
+        term['pageWidth'] = self.width
+        term['pageHeight'] = self.height
+        term['pageNumber'] = int(data[1])
+        term['left'] = int(data[6])
+        term['top'] = int(data[7])
+        term['right'] = self.width - (int(data[6]) + int(data[8]))
+        term['bottom'] = self.height - (int(data[7]) + int(data[9]))
+        return term
+
+image_path = sys.argv[1]
+pdfb = pdf2bbox(image_path)
+print(pdfb.header)
+print(pdfb.width, pdfb.height)
+print(pdfb.data)
