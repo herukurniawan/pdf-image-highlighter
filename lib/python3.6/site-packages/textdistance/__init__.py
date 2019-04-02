19561723d18ce611461a2f9d8cdb057ddd63c500
herukurniawan <herupurnomokurniawan@gmail.com>
2019-04-02 21:32:54 +0700

    hamming library

---
+++ lib/python3.6/site-packages/textdistance/__init__.py
@@ -0,0 +1,20 @@
+"""
+TextDistance.
+Compute distance between sequences.
+30+ algorithms, pure python implementation, common interface.
+"""
+
+# main package info
+__title__ = 'TextDistance'
+__version__ = '3.0.2'
+__author__ = 'Gram Orsinium'
+__license__ = 'LGPL 3.0'
+
+
+# version synonym
+VERSION = __version__
+
+
+# app
+from .algorithms import *  # noQA
+from .utils import *  # noQA
